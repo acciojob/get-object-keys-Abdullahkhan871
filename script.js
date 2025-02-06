d452ecc6a29ed10fe49440fc28752405c282e962
Abdullahkhan871 <123239280+Abdullahkhan871@users.noreply.github.com>
2025-02-06 10:02:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-function getKeys(obj){
-	return Object.keys(obj)
-}
-
+// Create the student object
 const student = {
   name: "John",
 };
 
-console.log(getKeys(student))
+// Function to get all keys from the object
+function getKeys(obj) {
+  return Object.keys(obj);
+}
+
+// Testing the function
+console.log(getKeys(student)); // Output: ["name"]
